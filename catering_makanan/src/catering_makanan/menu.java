9afac9709432f7f24b3c0b4d61178ddaca7b7745
nurhaidah346 <63896736+nurhaidah346@users.noreply.github.com>
2020-04-23 14:54:29 +0700

    update isian menu

--- catering_makanan/src/catering_makanan/menu.java
+++ catering_makanan/src/catering_makanan/menu.java
@@ -10,18 +10,53 @@ public class menu {
         System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
     }
     public void menu2(){
-        System.out.println("Dalam Menu 1 Anda akan mendapatkan:");
+        System.out.println("Dalam Menu 2 Anda akan mendapatkan:");
         System.out.println(" 1. Nasi putih \n 2. Ayam Balado \n 3. Telor rebus");
         System.out.println(" 4. Kikil \n 5. Sambal \n 6. Kerupuk udang \n 7. Air mineral");
         System.out.println("Harga 25 ribu/pcs");
         System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
     }
     public void menu3(){
-        System.out.println("Dalam Menu 1 Anda akan mendapatkan:");
+        System.out.println("Dalam Menu 3 Anda akan mendapatkan:");
         System.out.println(" 1. Nasi Tumpeng \n 2. Ayam Balado \n 3. Telor rebus");
         System.out.println(" 4. Kikil \n 5. Sambal \n 6. Kerupuk udang \n 7. Oseng mie");
         System.out.println(" 8. Kering tempe \n 9. Mendoan");
         System.out.println("Harga 25 ribu/pcs");
+        System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
+    }
+    public void menu4(){
+        System.out.println("Dalam Menu 4 Anda akan mendapatkan:");
+        System.out.println(" 1. Nasi putih \n 2. Ayam Bakar \n 3. Telor");
+        System.out.println(" 4. Lalapan \n 5. Sambal \n 6. Es Teh");
+        System.out.println("Harga 15 ribu/pcs");
+        System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
+    }
+    public void menu5(){
+        System.out.println("Dalam Menu 5 Anda akan mendapatkan:");
+        System.out.println(" 1. Nasi putih \n 2. Ayam Kremes \n 3. Cah Kangkung");
+        System.out.println(" 4. Sambal \n 5. Es Teh \n 6. Kerupuk");
+        System.out.println("Harga 17 ribu/pcs");
+        System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
+    }
+    public void menu6(){
+        System.out.println("Dalam Menu 6 Anda akan mendapatkan:");  
+        System.out.println(" 1. Nasi Uduk \n 2. Ayam Rica \n 3. Tempe");
+        System.out.println(" 4. Telor \n 5. Sambal Terasi \n 6. Es Teh");
+        System.out.println("Harga 20 ribu/pcs");
+        System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
+    }
+    public void menu7(){
+        System.out.println("Dalam Menu 7 Anda akan mendapatkan:");
+        System.out.println(" 1. Nasi \n 2. Mangut Lele");
+        System.out.println(" 3. Kerupuk \n 4. Es Teh");
+        system.out.println("Harga 15 ribu/pcs");
+        System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
+    }
+    public void menu8(){
+        System.out.println("Dalam Menu 8 Anda akan mendapatkan:");
+        System.out.println(" 1. Nasi \n 2. Lele Bakar \n 3. Lalapan");
+        System.out.println(" 4. Sambal Matah \n 5. Es Teh");
+        system.out.println("Harga 18 ribu/pcs");
         System.out.print("<1> Untuk membeli\n<0> Keluar\nPilih: ");
     }
 }
